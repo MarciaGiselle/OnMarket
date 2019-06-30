9a53761977d149ab883fd4d7c404ee6221f82bf9
MarciaGiselle <margisetoledo@gmail.com>
2019-06-30 14:05:42 -0300

    Validaciones en tarjeta

--- TrabajoDePrograWeb2/Codigo Fuente/src/Webroot/js/comprar.js
+++ TrabajoDePrograWeb2/Codigo Fuente/src/Webroot/js/comprar.js
@@ -8,6 +8,25 @@ var inputcodigo = $("#codigoDeSeguridad");
 var inputfecha = $("#fechaDeVencimiento");
 var inputtotal = $('#total');
 
+function validarNumeroTarjeta(){
+    var validacion = false;
+    var numero = inputnumeroTarjeta.val();
+    if(numero === null || numero.length === 0 || numero === "") {
+        $("#errorNumero").removeClass("d-none").addClass("d-flex").find("small").text("Ingrese un número de tarjeta");
+        $("#errorNumero").fadeIn("slow");
+    } else if ((numero.length) != 16){
+        $("#errorNumero").removeClass("d-none").addClass("d-flex").find("small").text("El número de tarjeta debe tener 16 digitos");
+        $("#errorNumero").fadeIn("slow");
+    } else if(!regexNumeros.test(numero)) {
+        $("#errorNumero").removeClass("d-none").addClass("d-flex").find("small").text("El número de tarjeta sólo puede contener números");
+        $("#errorNumero").fadeIn("slow");
+    } else{
+        validacion = true;
+    }
+    return validacion;
+}
+
+
 function validarCodigo(){
     var validacion = false;
     var codigo = inputcodigo.val();
@@ -17,10 +36,9 @@ function validarCodigo(){
     } else if ((codigo.length) != 3){
         $("#errorCodigo").removeClass("d-none").addClass("d-flex").find("small").text("El código de seguridad debe tener 3 digitos");
         $("#errorCodigo").fadeIn("slow");
-    }
-    else if(!regexNumeros.test(codigo)) {
-      $("#errorNombre").removeClass("d-none").addClass("d-flex").find("small").text("El codigo de seguridad debe ser numérico");
-      $("#errorNombre").fadeIn("slow");
+    } else if(!regexNumeros.test(codigo)) {
+      $("#errorCodigo").removeClass("d-none").addClass("d-flex").find("small").text("El codigo de seguridad debe ser numérico");
+      $("#errorCodigo").fadeIn("slow");
     } else {
         validacion = true;
     }
@@ -30,15 +48,23 @@ function validarCodigo(){
 }
 
 
+
 function validarFecha(){
+
     var validacion = false;
     var fecha = inputfecha.val();
+    var fechaVenci= new Date(fecha);
+    var hoy = new Date();
+    var dd = hoy.getDate();
+
     if(fecha === null || fecha.length === 0 || fecha === "") {
-      //  $("#errorNombre").removeClass("d-none").addClass("d-flex").find("small").text("Complete la fecha de vencimiento");
-        //$("#errorNombre").fadeIn("slow");
-    }else
-    {
-
+       $("#errorFecha").removeClass("d-none").addClass("d-flex").find("small").text("Ingrese la fecha de vencimiento");
+       $("#errorFecha").fadeIn("slow");
+    }else if(hoy.getTime()>fechaVenci.getTime()){
+        $("#errorFecha").removeClass("d-none").addClass("d-flex").find("small").text("Ingrese una fecha válida");
+        $("#errorFecha").fadeIn("slow");
+    }
+    else{
 
         validacion = true;
     }
@@ -47,37 +73,26 @@ function validarFecha(){
 
 }
 
-function validarNumeroTarjeta(){
-
-    var validacion = false;
-    var numero = inputnumeroTarjeta.val();
-    if(numero === null || numero.length === 0 || numero === "") {
-      //  $("#errorNombre").removeClass("d-none").addClass("d-flex").find("small").text("Complete la fecha de vencimiento");
-        //$("#errorNombre").fadeIn("slow");
-    } else if(!regexNumeros.test(numero)) {
-        //  $("#errorNombre").removeClass("d-none").addClass("d-flex").find("small").text("El codigo de seguirad debe ser numerico");
-        // $("#errorNombre").fadeIn("slow");
-    } else{
-
-
-        validacion = true;
-    }
-
-    return validacion;
-
-}
 
 
 
 
 confirmar.click(function () {
 
-    if(validarCodigo()){
+    if(validarNumeroTarjeta() ){
+        $(".error").fadeOut();
+        $(".error").removeClass("d-flex").addClass("d-none").find("span").text("");
+    }
+    if(validarCodigo() ){
+        $(".error").fadeOut();
+        $(".error").removeClass("d-flex").addClass("d-none").find("span").text("");
+    }
+    if(validarFecha() ){
         $(".error").fadeOut();
         $(".error").removeClass("d-flex").addClass("d-none").find("span").text("");
     }
 
-    var validacion = validarCodigo();
+    var validacion = validarCodigo() && validarNumeroTarjeta() && validarFecha();
     if(validacion) {
         var obj = {};
         obj.total = inputtotal.val();
