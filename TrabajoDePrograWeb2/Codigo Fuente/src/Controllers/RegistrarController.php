f2fbd8eed8f85ea24b26eabd0ad3c8e469003a22
MarciaGiselle <margisetoledo@gmail.com>
2019-07-11 20:43:55 -0300

    crear cuenta

--- TrabajoDePrograWeb2/Codigo Fuente/src/Controllers/RegistrarController.php
+++ TrabajoDePrograWeb2/Codigo Fuente/src/Controllers/RegistrarController.php
@@ -1,6 +1,7 @@
 <?php
+
 class RegistrarController extends Controller
-{    
+{
 
     function registrar()
     {
@@ -10,83 +11,77 @@ class RegistrarController extends Controller
 
     }
 
-     function validarRegistro($datosUsuario){
-         header("Content-type: application/json");
-         $datos = json_decode(utf8_decode($datosUsuario['data']));
+    function validarRegistro($datosUsuario)
+    {
+        header("Content-type: application/json");
+        $datos = json_decode(utf8_decode($datosUsuario['data']));
 
 
-        $usuario=new Usuario();
-        $rol= new Rol();
-         $usuario->setName($datos->nombre);
-         $usuario->setLastname($datos->apellido);
-         $usuario->setUserName($datos->nombreUsuario);
+        $usuario = new Usuario();
+        $rol = new Rol();
+        $usuario->setName($datos->nombre);
+        $usuario->setLastname($datos->apellido);
+        $usuario->setUserName($datos->nombreUsuario);
         // $usuario->setPassword($datos->pass);
-         $usuario->setCuit($datos->cuit);
-         $usuario->setSexo($datos->sexo);
-         $usuario->setEmail($datos->correo);
+        $usuario->setCuit($datos->cuit);
+        $usuario->setSexo($datos->sexo);
+        $usuario->setEmail($datos->correo);
 
 
-       if(isset($datos->terminos)){
-        $terminosYcondiciones=$datos->terminos;
-        }else{
-            $terminosYcondiciones="no";
+        if (isset($datos->terminos)) {
+            $terminosYcondiciones = $datos->terminos;
+        } else {
+            $terminosYcondiciones = "no";
         }
 
-        $rolASetear=$rol->determinarRol();
-       $usuario->setRol($rolASetear);
+        $rolASetear = $rol->determinarRol();
+        $usuario->setRol($rolASetear);
 
-        $pass=$datos->pass;
-        $pass2=$datos->pass2;
+        $pass = $datos->pass;
+        $pass2 = $datos->pass2;
 
-           if (!$usuario->consultarUserName()) {
+        if (!$usuario->consultarUserName()) {
             throw new ExcentionRegistar("Nombre de usuario ya existente", CodigoError::ExcentionRegistar);
-          }else if ($pass==$pass2) {
-                  //creo su logalizacion para luego setearla
-                  $localizacion=new Localizacion();
-                  $localizacion->setLatitud($datos->lat);
-                  $localizacion->setLongitud($datos->lon);
+        } else if ($pass == $pass2) {
+            //creo su logalizacion para luego setearla
+            $localizacion = new Localizacion();
+            $localizacion->setLatitud($datos->lat);
+            $localizacion->setLongitud($datos->lon);
 
 
+            $passSHA = sha1($pass2);
+            $usuario->setPassword($passSHA);
+            $usuario->setEstado(1);
+            $id_user = $usuario->insertarRegistro();
+            $_SESSION["logueado"] = $id_user;
 
+            //Crear cuenta
+            $this->crearCuenta($id_user);
 
-                  $passSHA = sha1($pass2);
-                  $usuario->setPassword($passSHA);
-
-                  $usuario->setEstado(1);
-
-                      $id_user= $usuario->insertarRegistro();
-                      $_SESSION["logueado"]=$id_user;
-
-                      if(isset($id_user)){
-                      $localizacion->setIdUser($id_user);
-                      $localizacion->insertarLocalizacion();
-                      }
-
-               }else{
-                throw new ExcentionRegistar("las contraseñas no son iguales", CodigoError::ExcentionRegistar);
-
+            if (isset($id_user)) {
+                $localizacion->setIdUser($id_user);
+                $localizacion->insertarLocalizacion();
             }
 
+        } else {
+            throw new ExcentionRegistar("las contraseñas no son iguales", CodigoError::ExcentionRegistar);
 
-            echo json_encode(true);
+        }
+
+
+        echo json_encode(true);
     }
 
 
-
-
-    function redireccionarALaPaginaDelUsuario(){
-            $this->render(Constantes::INDEXVIEW);
+    function redireccionarALaPaginaDelUsuario()
+    {
+        $this->render(Constantes::INDEXVIEW);
 
     }
 
-    /**
-     * @return bool
-     */
+    function crearCuenta($id){
 
 
+    }
 
-  
-
-    
-    
 }
---
+++ TrabajoDePrograWeb2/Codigo Fuente/src/Models/Cuenta.php
@@ -0,0 +1,96 @@
+<?php
+
+
+class Cuenta extends Model
+{
+    private $id;
+    private $fecha_deposito;
+    private $monto;
+    private $comisionAlSistema;
+    private $idUsuario;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFechaDeposito()
+    {
+        return $this->fecha_deposito;
+    }
+
+    /**
+     * @param mixed $fecha_deposito
+     */
+    public function setFechaDeposito($fecha_deposito)
+    {
+        $this->fecha_deposito = $fecha_deposito;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMonto()
+    {
+        return $this->monto;
+    }
+
+    /**
+     * @param mixed $monto
+     */
+    public function setMonto($monto)
+    {
+        $this->monto = $monto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComisionAlSistema()
+    {
+        return $this->comisionAlSistema;
+    }
+
+    /**
+     * @param mixed $comisionAlSistema
+     */
+    public function setComisionAlSistema($comisionAlSistema)
+    {
+        $this->comisionAlSistema = $comisionAlSistema;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdUsuario()
+    {
+        return $this->idUsuario;
+    }
+
+    /**
+     * @param mixed $idUsuario
+     */
+    public function setIdUsuario($idUsuario)
+    {
+        $this->idUsuario = $idUsuario;
+    }
+
+
+
+
+
+}
